4b52df1f7e7e0e41c7469b764539127148c41c66
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-04-14 01:44:17 +0800

    working front (history, requested room, homepage, book room) both jsx/css

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <AuthContextProvider>
         <Routes>
-          <Route path="/" index element={<Login />} />
+          <Route path="/login" index element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route
             path="/home"
