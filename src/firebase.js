e008b0af10f1f24e49bd9b794e92937c63f1987c
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-04-15 03:22:16 +0800

    added firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,9 +13,11 @@ const firebaseConfig = {
   storageBucket: "online-booking-osad.appspot.com",
   messagingSenderId: "858019033519",
   appId: "1:858019033519:web:2b88e0c64e604f02057f5a"
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export default app;
