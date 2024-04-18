0a6c8135af975b5880d0860f3a7d935875d6fda8
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-04-19 00:23:09 +0800

    can read displayName from authenticated user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import { getDocs, collection, query, where } from "firebase/firestore"; // Import Firestore functions
 
 import uicLogo from "../assets/logo.png";
 import "../styles/login.css";
+import { db } from "../firebase";
 
 const Login = () => {
   const navigate = useNavigate();
-  const { signIn } = UserAuth();
+  const { signIn, updateProfile } = UserAuth();
 
-  // const [user, setUser] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errMsg, setErrMsg] = useState("");
@@ -18,18 +19,46 @@ const Login = () => {
     e.preventDefault();
     setErrMsg("");
     try {
+      // Sign in the user
       await signIn(email, password);
+
+      // Fetch full name from Firestore based on email
+      const fullName = await fetchFullNameByEmail(email);
+
+      // Update user profile with full name
+      await updateProfile(fullName);
+
       navigate("/home");
-    } catch (e) {
-      console.log(e.message);
+    } catch (error) {
+      console.log(error.message);
+      setErrMsg("Invalid Credentials. Try again!");
     }
-    return setErrMsg("Invalid Credentials. Try again!" );
+  };
+
+  // Function to fetch full name from Firestore based on email
+  const fetchFullNameByEmail = async (email) => {
+    try {
+      const usersRef = collection(db, "users");
+      const q = query(usersRef, where("email", "==", email));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        throw new Error("User not found");
+      }
+
+      // Assuming there's only one user with the given email
+      const userData = querySnapshot.docs[0].data();
+      return userData.fullName;
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      throw error; // Propagate the error
+    }
   };
 
   return (
     <>
       <div className="error-container-login">
-        {/* error message */}
+        {/* Error message */}
         <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
           {errMsg}
         </p>
@@ -72,10 +101,10 @@ const Login = () => {
           </form>
         </div>
         <p className="signup-container">
-          Dont have an Account?
+          Don`&apos;`t have an Account?
           <br />
           <span className="line">
-            {/* put router link here */}
+            {/* Put router link here */}
             <Link to="/register">Sign up</Link>
           </span>
         </p>
