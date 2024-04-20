db5b33d878b49773cbf63b6b6f3c80188b845405
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-04-20 22:38:46 +0800

    added firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBeg9rYEaSTa1HcgsssRDyVTmaDpWIoZOM",
   authDomain: "online-booking-osad.firebaseapp.com",
@@ -13,11 +11,11 @@ const firebaseConfig = {
   storageBucket: "online-booking-osad.appspot.com",
   messagingSenderId: "858019033519",
   appId: "1:858019033519:web:2b88e0c64e604f02057f5a"
-
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 export default app;
