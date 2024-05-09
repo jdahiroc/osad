80309f37cf388718910806dd7cce1841ea35d3fa
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-05-09 15:12:41 +0800

    removed email js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-import emailjs from "emailjs-com";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeg9rYEaSTa1HcgsssRDyVTmaDpWIoZOM",
@@ -14,13 +13,10 @@ const firebaseConfig = {
   messagingSenderId: "858019033519",
   appId: "1:858019033519:web:2b88e0c64e604f02057f5a",
 };
-// Initialize EmailJS with your service ID and template ID
-emailjs.init("Ef5C26L9NQaFBx9R_");
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const emailJS = emailjs;
 export default app;
